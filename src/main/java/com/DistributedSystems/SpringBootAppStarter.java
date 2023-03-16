04c5a19873d3a36cf6bc386df0e9930d81e9e0aa
Shubham Kumar <shubham7.8.96@gmail.com>
2023-03-15 21:48:33 -0400

    Update SpringBootAppStarter.java

--- src/main/java/com/DistributedSystems/SpringBootAppStarter.java
+++ src/main/java/com/DistributedSystems/SpringBootAppStarter.java
@@ -7,7 +7,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-
+ @RestController
 public class SpringBootAppStarter extends SpringBootServletInitializer {
 
     @Override
